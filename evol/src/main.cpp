5964b1ef020a56edb14679484d09e251ade55a89
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2021-12-12 01:44:40 +0300

    Fixed solver type for solve

--- evol/src/main.cpp
+++ evol/src/main.cpp
@@ -53,19 +53,23 @@ int main(int argc, char** argv) {
   ea::Logger logger(argv[0], ea::config::global_config().logger_config());
 
   LOG(INFO) << "Input file: " << input;
+  ea::sat::RSolver solver =
+      ea::registry::Registry::resolve_solver(ea::config::global_config().solver_type());
+  LOG_TIME(solver->parse_cnf(input));
 
   if (backdoor) {
     ea::algorithm::RAlgorithm algorithm(
         ea::registry::Registry::resolve_algorithm(ea::config::global_config().algorithm_type()));
-    auto& solver = algorithm->get_solver();
-    LOG_TIME(solver.parse_cnf(input));
+    auto& algorithm_solver = algorithm->get_solver();
+    LOG_TIME(algorithm_solver.parse_cnf(input));
     algorithm->prepare();
 
     ea::SigHandler sig_handler;
     ea::SigHandler::CallbackHandle alg_int_handle =
-        sig_handler.register_callback([&algorithm](int) {
+        sig_handler.register_callback([&alg_int_handle, &algorithm](int) {
           algorithm->interrupt();
           LOG(INFO) << "Algorithm has been interrupted.";
+          alg_int_handle.remove();
         });
 
     LOG_TIME(algorithm->process());
@@ -73,31 +77,27 @@ int main(int argc, char** argv) {
     LOG(INFO) << "Number of points visited: " << algorithm->inaccurate_points();
     LOG(INFO) << "The best backdoor is: " << r_backdoor;
 
-    alg_int_handle.remove();
-    sig_handler.unset();
-
-    bool interrupted = false;
-    ea::SigHandler::CallbackHandle slv_int_handle =
-        sig_handler.register_callback([&solver, &interrupted](int) {
-          solver.interrupt();
-          interrupted = true;
-          LOG(INFO) << "Solver has been interrupted.";
-        });
-
+    std::atomic_bool satisfied = false, unknown = false;
+    std::atomic_uint64_t propagated{0}, total{0};
+    size_t num_vars = r_backdoor.fitness().pow_r;
     std::vector<bool> vars = r_backdoor.get_vars().get_mask();
     ea::domain::UAssignment assignment_p =
         ea::domain::createFullSearch(r_backdoor.var_view(), vars);
-
-    std::atomic_bool satisfied = false, unknown = false;
-    size_t num_vars = r_backdoor.fitness().pow_r;
-
     std::mutex progress_lock;
     boost::timer::progress_display progress((unsigned long) std::pow(2UL, num_vars), std::cerr);
 
+    bool interrupted = false;
+    sig_handler.unset();
+    ea::SigHandler::CallbackHandle slv_int_handle =
+        sig_handler.register_callback([&slv_int_handle, &solver, &interrupted](int) {
+          solver->interrupt();
+          interrupted = true;
+          LOG(INFO) << "Solver has been interrupted.";
+          slv_int_handle.remove();
+        });
+
     // clang-format off
-    std::atomic_uint64_t propagated{0};
-    std::atomic_uint64_t total{0};
-    solver.solve_assignments(
+    solver->solve_assignments(
         std::move(assignment_p), [&propagated, &total, &sig_handler, &satisfied, &unknown, &progress, &progress_lock](
           ea::sat::State result, bool prop_conflict, auto) {
             if (sig_handler.is_set()) {
@@ -123,6 +123,7 @@ int main(int argc, char** argv) {
                 return false;
             }
         });
+    sig_handler.unset();
     // clang-format on
     LOG(INFO) << "Prop: " << propagated << ", total: " << total;
     LOG(INFO) << "Actual rho is: " << (double) propagated / (double) total;
@@ -134,19 +135,17 @@ int main(int argc, char** argv) {
     } else {
       result = ea::sat::UNKNOWN;
     }
-    slv_int_handle.remove();
   } else {
-    ea::sat::RSolver solver =
-        ea::registry::Registry::resolve_solver(ea::config::global_config().solver_type());
     LOG_TIME(solver->parse_cnf(input));
     ea::SigHandler sig_handler;
-    ea::SigHandler::CallbackHandle slv_int_handle = sig_handler.register_callback([&solver](int) {
-      solver->interrupt();
-      LOG(INFO) << "Solver has been interrupted.";
-    });
-
+    ea::SigHandler::CallbackHandle slv_int_handle =
+        sig_handler.register_callback([&slv_int_handle, &solver](int) {
+          solver->interrupt();
+          LOG(INFO) << "Solver has been interrupted.";
+          slv_int_handle.remove();
+        });
     LOG_TIME(result = solver->solve_limited());
-    slv_int_handle.remove();
+    sig_handler.unset();
   }
 
   if (result == ea::sat::UNSAT) {
