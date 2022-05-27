366c854d9f7f90fb35ff783dfa23f7148c70f32e
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2022-05-27 15:42:04 +0300

    ps-sc-best-b

--- scripts/satcomp/run/solve.sh
+++ scripts/satcomp/run/solve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CONFIG="ms-rr-best-a.json"
-#CONFIG="ps-sc-best-b.json"
+#CONFIG="ms-rr-best-a.json"
+CONFIG="ps-sc-best-b.json"
 CNF_PATH="$1"
 
 GLOG_logtostderr=1 /itmo-parsat/build/cli/solve_bin \
